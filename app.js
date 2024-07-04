0f70ab6653dec18bbe8312149f06299c6baafa51
Mahesh20dev <ycmahesh86@gmail.com>
2024-07-04 12:59:09 +0530

    updates

---
+++ app.js
@@ -0,0 +1,179 @@
+let menuIcon=document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+       /**
+ * Calculate the scroll position and section properties.
+ *
+ * @returns {void}
+ */
+window.onscroll = () => {
+    // Loop through each section element
+    sections.forEach(sec => {
+        // Get the current scroll position
+        let top = window.scrollY;
+
+        // Calculate the offset from the top of the page to the section
+        let offset = sec.offsetTop - 150;
+
+        // Get the height of the section
+        let height = sec.offsetHeight;
+
+        // Get the id of the section
+        let id = sec.getAttribute('id');
+
+        // Check if the scroll position is within the section
+        if (top >= offset && top < offset + height) {
+            // Loop through each navigation link
+            navlinks.forEach(link => {
+                // Remove the 'active' class from all links
+                link.classList.remove('active');
+
+                // Add the 'active' class to the link that corresponds to the current section
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY>100);
+
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+ });
+
+ ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ ScrollReveal().reveal('.home-img,.services-container,.projects-box,.contact form',{origin:'bottom'});
+ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+ ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
+
+
+ const typed= new Typed('.multiple-text',{
+    strings:['Student','Frontend Developer','Programmer'],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true,
+    backDelay:1000
+    
+ })
+
+
+ const form=document.querySelector('form');
+ const fullName=document.getElementById("name");
+ const email=document.getElementById("email");
+ const phone=document.getElementById("phone");
+ const subject=document.getElementById("subject");
+ const mess=document.getElementById("message");
+
+ function sendEmail(){
+    const bodyMessage=`Full Name:${fullName.value}<br> Email:${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+
+        Email.send({
+            
+            SecureToken:"733450c8-470d-4959-b6d1-d19034b7de3d",
+            To : 'myc0382@gmail.com',
+            From : "myc0382@gmail.com",
+            Subject : subject.value,
+            Body : bodyMessage
+        }).then(message=>{
+            if (message === "OK"){
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent successfully!",
+                    icon: "success"
+                });
+                form.reset();
+            }else{
+                Swal.fire({
+                    title: "Oops!",
+                    text: "Something went wrong!",
+                    icon: "error"
+                });
+            }
+        })
+ };
+
+
+
+function checkInputs(){
+    const items=document.querySelectorAll(".item");
+
+    for (const item of items){
+        if (item.value ==""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value !=""){
+            checkEmail();
+        }
+        items[1].addEventListener("keyup",()=>{
+            checkEmail();
+        })
+
+        item.addEventListener("keyup",()=>{
+            if (item.value !=""){
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else{
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail(){
+    const emailRegex=/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errortxtemail=document.querySelector(".email")
+    if (!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+        if (email.value !=""){
+            errortxtemail.innerText="Enter a valid email Address";
+        }
+        else{
+            errortxtemail.innerText="Email Address can't be blank";
+        }
+    }
+    else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+    checkEmail();
+    checkInputs();
+    sendEmail();
+    form.reset();
+    //   alert("Your message has been sent successfully");
+    }
+  });
+
+
+
+
+
